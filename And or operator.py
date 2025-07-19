0ca9c8adfbcd2a50ca79c1215e6db28aadc2dd08
Jasraj2904 <jasrajsinghchatha@gmail.com>
2025-07-19 18:17:35 +0530

    Add files via upload

---
+++ And or operator.py
@@ -0,0 +1,6 @@
+d = int(input("Enter first number :"))
+j = int(input("Enter second number :"))
+if( d > 0 and j > 0):
+    print("Both Numbers are positive")
+else :
+    print("Numbers are not Positive")
