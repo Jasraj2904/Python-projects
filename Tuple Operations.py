943aee81b614dfb2067edda254f9171762e92539
Jasraj2904 <jasrajsinghchatha@gmail.com>
2025-09-13 18:20:37 +0530

    Add files via upload

---
+++ Tuple Operations.py
@@ -0,0 +1,9 @@
+tuple_x = ("string" , False , 12.5 , 12)
+print(tuple_x)
+tuple_1 = (6 , 11 , 11 , 11 , 11 , 25 , 32 , 42 , 52)
+print(tuple_1)
+tuple_1 = tuple_1 + (9 , )
+print(tuple_1)
+print(tuple_1.count(11))
+_slice = tuple_1[3:5]
+print(_slice)
