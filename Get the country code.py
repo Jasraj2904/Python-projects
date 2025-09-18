1422a5df28ee4c37d410ef2edd5a86decabf4d7f
Jasraj2904 <jasrajsinghchatha@gmail.com>
2025-09-18 19:34:52 +0530

    Add files via upload

---
+++ Get the country code.py
@@ -0,0 +1,9 @@
+country_code = {'India' : '0091' , 'Austraia' : '0025' , 'Nepal' : '00977'}
+print("Country Code for India - ")
+print(country_code.get('India' , 'not found'))
+
+print("Country Code for England - ")
+print(country_code.get('England' , 'not found'))
+
+print("Country Code for Canada - ")
+print(country_code.get('Canada' , 'not found'))
